8a2cd5da61e77db819be3098482f17cf5aadbbff
Giacomo Rebonato <grebonato@outlook.ie>
2020-04-12 14:10:34 +0100

    Removed burger

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,8 @@
-import { Box, Flex, Heading, Text } from '@chakra-ui/core'
+import { Box, Flex, Heading } from '@chakra-ui/core'
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
-const MenuItems: React.FC = ({ children }) => (
-  <Text mt={{ base: 4, md: 0 }} mr={6} display='block'>
-    {children}
-  </Text>
-)
-
 export const Header: React.FC = (props) => {
-  const [show, setShow] = React.useState(false)
-  const handleToggle = () => setShow(!show)
-
   return (
     <Flex
       as='nav'
@@ -29,27 +20,8 @@ export const Header: React.FC = (props) => {
         </Heading>
       </Flex>
 
-      <Box display={{ sm: 'block', md: 'none' }} onClick={handleToggle}>
-        <svg
-          fill='white'
-          width='12px'
-          viewBox='0 0 20 20'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <title>Menu</title>
-          <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
-        </svg>
-      </Box>
-
-      <Box
-        display={{ sm: show ? 'block' : 'none', md: 'flex', xs: 'none' }}
-        width={{ sm: 'full', md: 'auto' }}
-        alignItems='center'
-        flexGrow={1}
-      >
-        <MenuItems>
-          <NavLink to='/about'>About</NavLink>
-        </MenuItems>
+      <Box display='flex' width='auto' alignItems='center' flexGrow={1}>
+        <NavLink to='/about'>About</NavLink>
       </Box>
     </Flex>
   )
