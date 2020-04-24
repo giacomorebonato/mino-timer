c367c1d6bbc7b93245d0454520585f3685991301
Giacomo Rebonato <grebonato@outlook.ie>
2020-04-24 16:06:26 +0100

    Added Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
+import * as Sentry from '@sentry/browser'
 import 'mobx-react-lite/batchingForReactDom'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+
+Sentry.init({
+  dsn:
+    'https://21d638c5eda142e58407e366dd4d6684@o382969.ingest.sentry.io/5212603'
+})
 
 ReactDOM.render(
   <React.StrictMode>
